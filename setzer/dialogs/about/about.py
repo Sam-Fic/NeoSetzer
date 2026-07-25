3a7f7ae4470ab27969d617ec8eb51ce5ee37ee03
Sam-Fic <2401894494@qq.com>
2026-07-25 22:02:54 +0800

    feat(about): add fork maintainer credits and link to fork repository
    - Keep original author (Robert Griesel) as developer and website
    - Add 'Fork maintainer' credits section with clickable GitHub link
    - Add 'Fork Repository' link on Details page pointing to Sam-Fic/Setzer
    - Update copyright, support and issue URLs to the fork repository

--- setzer/dialogs/about/about.py
+++ setzer/dialogs/about/about.py
@@ -40,18 +40,24 @@ class AboutDialog(object):
         self.view.set_application_icon('org.cvfosammmm.Setzer')
         self.view.set_version(ServiceLocator.get_setzer_version())
         self.view.set_developer_name('Robert Griesel')
-        self.view.set_copyright('© 2017-present Robert Griesel')
+        self.view.set_copyright('© 2017-present Robert Griesel, Sam-Fic')
         self.view.set_comments(_('Setzer is a LaTeX editor.'))
         self.view.set_license_type(Gtk.License.GPL_3_0)
         self.view.set_website('https://www.cvfosammmm.org/setzer/')
-        self.view.set_support_url('https://github.com/cvfosammmm/Setzer/discussions')
-        self.view.set_issue_url('https://github.com/cvfosammmm/Setzer/issues')
+        self.view.set_support_url('https://github.com/Sam-Fic/Setzer/discussions')
+        self.view.set_issue_url('https://github.com/Sam-Fic/Setzer/issues')
+        self.view.add_link(_('Fork Repository'), 'https://github.com/Sam-Fic/Setzer')
+        # libadwaita 的 set_release_notes() 要求内容是合法 XML（含根元素）
+        # 的 HTML 子集，裸纯文本无 <p>/<ul> 根会被解析器拒绝
+        # （"Document must begin with an element"）。用 <ul><li> 包裹成列表。
         self.view.set_release_notes(_('''
-• Performance improvements and bug fixes
-• Improved keyboard shortcuts
-• Added print functionality
-• Enhanced preview keyboard navigation
-• Better error handling for file operations'''))
+<ul>
+<li>Performance improvements and bug fixes</li>
+<li>Improved keyboard shortcuts</li>
+<li>Added print functionality</li>
+<li>Enhanced preview keyboard navigation</li>
+<li>Better error handling for file operations</li>
+</ul>'''))
         import platform
         debug_info = 'Setzer version: {}\nOS: {} {}\nPython: {}'.format(
             ServiceLocator.get_setzer_version(),
@@ -59,5 +65,8 @@ class AboutDialog(object):
             platform.python_version())
         self.view.set_debug_info(debug_info)
         self.view.set_developers(('Robert Griesel',))
+        # Fork maintainer: shown as a separate credits section with a clickable
+        # GitHub link. Adw.AboutDialog auto-detects URLs in credit entries.
+        self.view.add_credit_section(_('Fork maintainer'), ['Sam-Fic https://github.com/Sam-Fic'])
         # TRANSLATORS: 'Name <email@domain.com>' or 'Name https://website.example'
         self.view.set_translator_credits(_('translator-credits'))
