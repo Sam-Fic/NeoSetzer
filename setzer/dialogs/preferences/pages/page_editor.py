61bccada5d73b58b3b85d0f7a93669f11f99a8de
Sam-Fic <2401894494@qq.com>
2026-08-01 18:24:19 +0800

    refactor(autocomplete): remove legacy preferences page
    Remove the PageAutocomplete class and navigation key binding infrastructure from the autocomplete preferences page. This simplifies the preferences structure and addresses remaining items from issue #6 by moving toward a more maintainable implementation.

--- setzer/dialogs/preferences/pages/page_editor.py
+++ setzer/dialogs/preferences/pages/page_editor.py
@@ -179,6 +179,66 @@ class PageEditor(object):
         self.view.preview_source_view.set_pixels_below_lines(ls - ls // 2)
         self.view.preview_source_view.set_pixels_inside_wrap(ls)
 
+        # 自动补全设置初始化
+        self.view.option_autocomplete.set_active(self.settings.get_value('preferences', 'enable_autocomplete'))
+        self.view.option_autocomplete.connect('notify::active', self.on_switch_toggled, 'enable_autocomplete')
+
+        self.view.option_bracket_completion.set_active(self.settings.get_value('preferences', 'enable_bracket_completion'))
+        self.view.option_bracket_completion.connect('notify::active', self.on_switch_toggled, 'enable_bracket_completion')
+
+        self.view.option_selection_brackets.set_active(self.settings.get_value('preferences', 'bracket_selection'))
+        self.view.option_selection_brackets.connect('notify::active', self.on_switch_toggled, 'bracket_selection')
+
+        self.view.option_tab_jump_brackets.set_active(self.settings.get_value('preferences', 'tab_jump_brackets'))
+        self.view.option_tab_jump_brackets.connect('notify::active', self.on_switch_toggled, 'tab_jump_brackets')
+
+        self.view.option_update_matching_blocks.set_active(self.settings.get_value('preferences', 'update_matching_blocks'))
+        self.view.option_update_matching_blocks.connect('notify::active', self.on_switch_toggled, 'update_matching_blocks')
+
+        self.view.option_environment_autocomplete.set_active(self.settings.get_value('preferences', 'enable_environment_autocomplete'))
+        self.view.option_environment_autocomplete.connect('notify::active', self.on_switch_toggled, 'enable_environment_autocomplete')
+
+        accel = self.settings.get_value('preferences', 'autocomplete_manual_trigger')
+        self.view.trigger_button.set_label(self._accel_label(accel))
+        self.view.trigger_button.connect('clicked', self.on_trigger_capture_start)
+
+        # 补全弹窗导航键
+        for setting_name, title, subtitle in self._nav_key_rows():
+            row = Adw.ActionRow()
+            row.set_title(title)
+            row.set_subtitle(subtitle)
+            button = Gtk.Button()
+            button.set_valign(Gtk.Align.CENTER)
+            button.set_label(self._accel_label(self.settings.get_value('preferences', setting_name)))
+            button.connect('clicked', self.on_nav_capture_start, setting_name)
+            row.add_suffix(button)
+            row.set_activatable_widget(button)
+            self.view.nav_group.add(row)
+            self.view.nav_buttons[setting_name] = button
+
+    # ---- 自动补全设置（从 page_autocomplete 移入）----
+    def _nav_key_rows(self):
+        return [
+            ('autocomplete_previous', _('Select previous suggestion'),
+             _('Move the selection up one item in the completion popup.')),
+            ('autocomplete_next', _('Select next suggestion'),
+             _('Move the selection down one item in the completion popup.')),
+            ('autocomplete_previous_page', _('Previous page'),
+             _('Scroll the completion popup up one page.')),
+            ('autocomplete_next_page', _('Next page'),
+             _('Scroll the completion popup down one page.')),
+            ('autocomplete_accept', _('Accept suggestion'),
+             _('Insert the currently selected completion.')),
+            ('autocomplete_cancel', _('Dismiss popup'),
+             _('Close the completion popup without inserting anything.')),
+        ]
+
+    def _accel_label(self, accel):
+        _success, keyval, mods = Gtk.accelerator_parse(accel)
+        if keyval == 0:
+            return _('Disabled')
+        return Gtk.accelerator_get_label(keyval, mods)
+
     def _setup_combo_row(self, combo_row, pref_key, values):
         idx = values.index(self.settings.get_value('preferences', pref_key))
         combo_row.set_selected(idx)
@@ -188,12 +248,69 @@ class PageEditor(object):
         value = values[combo_row.get_selected()]
         self.settings.set_value('preferences', pref_key, value)
 
-
     def on_switch_toggled(self, switch, pspec, preference_name):
         self.settings.set_value('preferences', preference_name, switch.get_active())
         # 实时同步到预览 SourceView，让用户在偏好设置界面就能看到效果。
         if preference_name in ('show_line_endings', 'show_whitespace'):
             self._apply_preview_space_drawer()
+
+    def on_trigger_capture_start(self, button):
+        self._start_capture(button, 'autocomplete_manual_trigger', 'trigger')
+
+    def on_nav_capture_start(self, button, setting_name):
+        self._start_capture(button, setting_name, 'nav')
+
+    def _start_capture(self, button, setting_name, mode):
+        if self._capture_controller is not None:
+            self._cancel_capture()
+            return
+        self._capture_setting = setting_name
+        self._capture_mode = mode
+        self._capture_button = button
+        button.set_label(_('Press a shortcut… (Esc to cancel)'))
+        controller = Gtk.EventControllerKey()
+        controller.set_propagation_phase(Gtk.PropagationPhase.CAPTURE)
+        controller.connect('key-pressed', self.on_capture_keypress)
+        self.view.add_controller(controller)
+        self._capture_controller = controller
+
+    def on_capture_keypress(self, controller, keyval, keycode, state):
+        if keyval == Gdk.keyval_from_name('Escape'):
+            self._cancel_capture()
+            return True
+        mask = Gtk.accelerator_get_default_mod_mask()
+        if keyval == Gdk.keyval_from_name('space') and (state & mask) == 0:
+            return True
+        if Gtk.keyval_is_modifier(keyval):
+            return True
+        if self._capture_mode == 'trigger':
+            ignore = {'Tab', 'ISO_Left_Tab', 'Return', 'KP_Enter', 'Up', 'Down',
+                      'Left', 'Right', 'Page_Up', 'Page_Down', 'Home', 'End',
+                      'BackSpace', 'Delete'}
+            if keyval in {Gdk.keyval_from_name(n) for n in ignore}:
+                return True
+        accel = Gtk.accelerator_name(keyval, state & mask)
+        _success, kv, m = Gtk.accelerator_parse(accel)
+        if kv == 0:
+            self._cancel_capture()
+            return True
+        self.settings.set_value('preferences', self._capture_setting, accel)
+        self._capture_button.set_label(self._accel_label(accel))
+        self._end_capture()
+        return True
+
+    def _end_capture(self):
+        if self._capture_controller is not None:
+            self.view.remove_controller(self._capture_controller)
+            self._capture_controller = None
+        self._capture_setting = None
+        self._capture_mode = None
+        self._capture_button = None
+
+    def _cancel_capture(self):
+        accel = self.settings.get_value('preferences', self._capture_setting)
+        self._capture_button.set_label(self._accel_label(accel))
+        self._end_capture()
 
     def _apply_preview_space_drawer(self):
         '''将 show_line_endings / show_whitespace 设置同步到预览 SourceView。'''
@@ -407,6 +524,20 @@ class PageEditor(object):
         self.view.preview_source_view.set_pixels_inside_wrap(ls)
         # 重置编辑器字号缩放倍率为默认值 1.0
         self.settings.set_value('preferences', 'editor_font_zoom_level', 1.0)
+        # 重置自动补全设置
+        self.view.option_autocomplete.set_active(defaults['enable_autocomplete'])
+        self.view.option_bracket_completion.set_active(defaults['enable_bracket_completion'])
+        self.view.option_selection_brackets.set_active(defaults['bracket_selection'])
+        self.view.option_tab_jump_brackets.set_active(defaults['tab_jump_brackets'])
+        self.view.option_update_matching_blocks.set_active(defaults['update_matching_blocks'])
+        self.view.option_environment_autocomplete.set_active(defaults['enable_environment_autocomplete'])
+        self.view.trigger_button.set_label(self._accel_label(defaults['autocomplete_manual_trigger']))
+        self.settings.set_value('preferences', 'autocomplete_manual_trigger', defaults['autocomplete_manual_trigger'])
+        for name in ('autocomplete_previous', 'autocomplete_next',
+                     'autocomplete_previous_page', 'autocomplete_next_page',
+                     'autocomplete_accept', 'autocomplete_cancel'):
+            self.settings.set_value('preferences', name, defaults[name])
+            self.view.nav_buttons[name].set_label(self._accel_label(defaults[name]))
 
 
 class PageEditorView(Adw.PreferencesPage):
@@ -724,6 +855,59 @@ class PageEditorView(Adw.PreferencesPage):
         ])
         self.option_default_line_ending.set_model(line_ending_list)
         group_new_doc.add(self.option_default_line_ending)
+
+        # 自动补全设置（从 page_autocomplete 移入）
+        group_autocomplete = Adw.PreferencesGroup()
+        group_autocomplete.set_title(_('Autocomplete'))
+        self.add(group_autocomplete)
+
+        self.option_autocomplete = Adw.SwitchRow()
+        self.option_autocomplete.set_title(_('Suggest matching LaTeX Commands'))
+        self.option_autocomplete.set_subtitle(_('Show a completion popup as you type LaTeX commands.'))
+        group_autocomplete.add(self.option_autocomplete)
+
+        row_trigger = Adw.ActionRow()
+        row_trigger.set_title(_('Manual trigger key'))
+        row_trigger.set_subtitle(_('Press to capture a shortcut that opens the completion popup.'))
+        self.trigger_button = Gtk.Button()
+        self.trigger_button.set_valign(Gtk.Align.CENTER)
+        row_trigger.add_suffix(self.trigger_button)
+        group_autocomplete.add(row_trigger)
+
+        self.nav_group = Adw.PreferencesGroup()
+        self.nav_group.set_title(_('Completion Navigation'))
+        self.nav_group.set_description(_('Keys used to move and confirm within the completion popup.'))
+        self.add(self.nav_group)
+        self.nav_buttons = dict()
+
+        group_brackets = Adw.PreferencesGroup()
+        group_brackets.set_title(_('Brackets and Blocks'))
+        self.add(group_brackets)
+
+        self.option_bracket_completion = Adw.SwitchRow()
+        self.option_bracket_completion.set_title(_('Automatically add closing brackets'))
+        self.option_bracket_completion.set_subtitle(_('Insert a matching closing bracket when you type an opening one.'))
+        group_brackets.add(self.option_bracket_completion)
+
+        self.option_selection_brackets = Adw.SwitchRow()
+        self.option_selection_brackets.set_title(_('Add brackets to selected text, instead of replacing it with them'))
+        self.option_selection_brackets.set_subtitle(_('Wrap the selected text in brackets, keeping it selected.'))
+        group_brackets.add(self.option_selection_brackets)
+
+        self.option_tab_jump_brackets = Adw.SwitchRow()
+        self.option_tab_jump_brackets.set_title(_('Jump over closing brackets with Tab'))
+        self.option_tab_jump_brackets.set_subtitle(_('Press Tab to move past an automatically inserted closing bracket.'))
+        group_brackets.add(self.option_tab_jump_brackets)
+
+        self.option_update_matching_blocks = Adw.SwitchRow()
+        self.option_update_matching_blocks.set_title(_('Update matching begin / end blocks'))
+        self.option_update_matching_blocks.set_subtitle(_('Keep matching \\begin{} / \\end{} blocks in sync when you edit one.'))
+        group_brackets.add(self.option_update_matching_blocks)
+
+        self.option_environment_autocomplete = Adw.SwitchRow()
+        self.option_environment_autocomplete.set_title(_('Automatically add matching \\end{}'))
+        self.option_environment_autocomplete.set_subtitle(_('When you type \\begin{, automatically insert the matching \\end{} (use Tab to jump to the content placeholder).'))
+        group_brackets.add(self.option_environment_autocomplete)
 
         group_reset = Adw.PreferencesGroup()
         self.add(group_reset)
