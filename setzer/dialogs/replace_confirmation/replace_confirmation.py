03554a50d99273c3bdd9b3c63aa197af7d30fa76
Sam-Fic <2401894494@qq.com>
2026-08-03 05:56:50 +0800

    chore: 补充版权声明，更新各源

--- setzer/dialogs/replace_confirmation/replace_confirmation.py
+++ setzer/dialogs/replace_confirmation/replace_confirmation.py
@@ -2,6 +2,7 @@
 # coding: utf-8
 
 # Copyright (C) 2017-present Robert Griesel
+# Copyright (C) 2026 Sam-Fic
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
